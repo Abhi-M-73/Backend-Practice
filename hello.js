802ea5ef84923b7fc1605b0498fb62b465f6e69a
Abhishek <abhishekmaddheshiya632@gmail.com>
2024-09-15 22:39:23 +0530

    phase1 complete

---
+++ hello.js
@@ -0,0 +1,10 @@
+// const math = require('./math');
+
+// console.log(math.add(2,4));
+// console.log(math.sub(5,3));
+
+const {add, sub} = require('./math');
+
+console.log(add(2,4));
+console.log(sub(5,3));
+
---
+++ math.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+    return a + b;
+}
+
+const sub = (a, b) => {
+    return a - b;
+}
+
+
+// exports.add = (a, b)=> a + b; 
+// exports.sub = (a, b)=> a - b; 
+module.exports = {add, sub};       
